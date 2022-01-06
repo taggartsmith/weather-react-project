3322fd832d00464cf50c37f83608b6b97acd9ae6
Taggart Smith <taggart.smith@gmail.com>
2022-01-05 21:31:37 -0700

    Clean Up

--- src/components/weather.js
+++ src/components/weather.js
@@ -24,18 +24,6 @@ function Weatherfunction() {
   }
 
 
-  // const dateBuilder = (d) => {
-  //   let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-  //   let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-
-  //   let day = days[d.getDay()];
-  //   let date = d.getDate();
-  //   let month = months[d.getMonth()];
-  //   let year = d.getFullYear();
-
-  //   return `${day} ${month} ${date} ${year}`
-  // }
-
   return (
     <div className={(typeof weather.main != "undefined") ? ((weather.main.temp > 16) ? 'weather warm' : 'weather') : 'weather'}>
       <div className="date">
