381a61716019e6321a8c6884db1fe87a9a4ae811
Taggart Smith <taggart.smith@gmail.com>
2022-01-08 19:15:46 -0700

    first

--- src/components/slider-component/sliderData.js
+++ src/components/slider-component/sliderData.js
@@ -1,8 +1,8 @@
 import Picture1 from '../../../static/assets/images/picture1.jpg'
 import Picture2 from '../../../static/assets/images/picture.jpg'
 import Picture3 from '../../../static/assets/images/about-me.jpg'
-import Picture4 from '../../../static/assets/images/drone2.JPG'
-import Picture5 from '../../../static/assets/images/drone3.JPG'
+import Picture4 from '../../../static/assets/images/drone2.jpg'
+import Picture5 from '../../../static/assets/images/drone3.jpg'
 
 export const SliderData = [
     {
